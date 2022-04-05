d8a9b64aaa821dffcf91f6778c96b2e2a8667725
phaedra144 <bedi.szilvia@gmail.com>
2022-04-05 21:31:17 +0200

    init backend

---
+++ backend/src/main/java/com/polygence/spending/exercise/SpendingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.polygence.spending.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendingExerciseApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/polygence/spending/exercise/SpendingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polygence.spending.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
