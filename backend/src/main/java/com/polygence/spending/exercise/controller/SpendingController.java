80de593977f29372c4777184c3847dfa3281ea2d
phaedra144 <bedi.szilvia@gmail.com>
2022-04-05 23:55:02 +0200

    Add cross origin annotation, remove unnecessary db config

--- backend/src/main/java/com/polygence/spending/exercise/controller/SpendingController.java
+++ backend/src/main/java/com/polygence/spending/exercise/controller/SpendingController.java
@@ -4,6 +4,7 @@ import com.polygence.spending.exercise.model.SpendingItem;
 import com.polygence.spending.exercise.service.SpendingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/spendings")
 public class SpendingController {
