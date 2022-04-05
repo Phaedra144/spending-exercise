4976236ea269b6006870d42d3dcd26e0d5063958
phaedra144 <bedi.szilvia@gmail.com>
2022-04-05 23:05:37 +0200

    Add service with mapping

---
+++ backend/src/main/java/com/polygence/spending/exercise/service/SpendingService.java
@@ -0,0 +1,34 @@
+package com.polygence.spending.exercise.service;
+
+import com.polygence.spending.exercise.entity.SpendingEntity;
+import com.polygence.spending.exercise.model.SpendingItem;
+import com.polygence.spending.exercise.repository.SpendingRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SpendingService {
+
+    @Autowired
+    SpendingRepository spendingRepository;
+
+     ModelMapper mapper = new ModelMapper();
+    
+    public List<SpendingItem> getSpendings() {
+        return spendingRepository.findAll()
+                .stream().map(sp -> mapper.map(sp, SpendingItem.class))
+                .collect(Collectors.toList());
+    }
+
+    public SpendingItem getSpendingItemById(Long id) {
+        return mapper.map(spendingRepository.getById(id), SpendingItem.class);
+    }
+
+    public void createSpendingItem(SpendingItem spendingItem) {
+        spendingRepository.save(mapper.map(spendingItem, SpendingEntity.class));
+    }
+}
