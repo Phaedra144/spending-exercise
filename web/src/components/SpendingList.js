3c32d95d3896dc4855d067e43159aebea748f165
phaedra144 <bedi.szilvia@gmail.com>
2022-04-06 18:12:55 +0200

    Make use of custom use-http hook, refactor Spendinglist

--- web/src/components/SpendingList.js
+++ web/src/components/SpendingList.js
@@ -1,59 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Loader from "./Loader";
 import { ErrorMessage } from "../styles/ComponentStyles";
 import SpendingItem from "./SpendingItem";
+import { getAllSpendings } from "../lib/api";
+import useHttp from "../hooks/use-http";
 
-export default function SpendingList({ spendings, setSpendings }) {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+export default function SpendingList() {
+  const { sendRequest, data: loadedSpendings, error, status } = useHttp(getAllSpendings, true);
 
   useEffect(() => {
-    setLoading(true);
-    fetch(`http://localhost:8080/spendings`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    })
-      .then(async (res) => {
-        const body = await res.json();
-        return {
-          status: res.status,
-          body,
-        };
-      })
-      .then((response) => {
-        if (response.status === 200) {
-          setSpendings(response.body);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [setSpendings]);
+    sendRequest();
+  }, [sendRequest]);
 
-  if (loading) return <Loader />;
+  if (status === 'pending') {
+    return (
+      <Loader />
+    );
+  }
+
+  if (status === 'completed' && !error && (!loadedSpendings || !loadedSpendings.length)) {
+    return (
+      <h1 style={{ textAlign: "center", marginTop: "4rem" }}>
+        Yay!{" "}
+        <span role="img" aria-label="jsx-a11y/accessible-emoji">
+          🎉
+        </span>{" "}
+        No spendings!
+      </h1>
+    )
+  }
+
+  if (error) {
+    return (
+      <ErrorMessage>
+        The server is probably down. Please try again later.
+      </ErrorMessage>
+    );
+  }
 
   return (
     <>
-      {error && (
-        <ErrorMessage>
-          The server is probably down. Please try again later.
-        </ErrorMessage>
-      )}
-      {!spendings.length && !error && (
-        <h1 style={{ textAlign: "center", marginTop: "4rem" }}>
-          Yay!{" "}
-          <span role="img" aria-label="jsx-a11y/accessible-emoji">
-            🎉
-          </span>{" "}
-          No spendings!
-        </h1>
-      )}
-      {spendings.length > 0 &&
-        spendings.map((spending) => (
+      {loadedSpendings.length > 0 &&
+        loadedSpendings.map((spending) => (
           <SpendingItem
             key={spending.id}
             id={spending.id}
--- web/src/lib/api.js
+++ web/src/lib/api.js
@@ -1,4 +1,26 @@
-const BACKEND_DOMAIN = 'https://localhost:8080';
+const BACKEND_DOMAIN = 'http://localhost:8080';
+
+export async function getAllSpendings() {
+  const response = await fetch(`${BACKEND_DOMAIN}/spendings`);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Could not fetch spendings.');
+  }
+
+/*   const transformedSpendings = [];
+
+  for (const key in data) {
+    const spendingObj = {
+      id: key,
+      ...data[key],
+    };
+
+    transformedSpendings.push(spendingObj);
+  } */
+
+  return data;
+}
 
 export async function createSpending(spendingData) {
   const response = await fetch(`${BACKEND_DOMAIN}/spendings`, {
@@ -16,3 +38,6 @@ export async function createSpending(spendingData) {
 
   return null;
 }
+
+
+
